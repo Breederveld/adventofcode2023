6adb13ca99153f72ca93d49d7fb6bbdb18bac74f
Andrei Lazar <andrei.lazar@centric.eu>
2023-12-05 11:12:09 +0200

    trying to catch up

---
+++ andrei/AdventOfCode2023/Day01.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023
+{
+    internal class Day01
+    {
+        string input;
+
+        public Day01()
+        {
+            var currentAssembly = Assembly.GetExecutingAssembly();
+            var numbers = new List<int>();
+            using (var stream = currentAssembly.GetManifestResourceStream($"{currentAssembly.GetName().Name}.Inputs.Day01a.txt"))
+            using (var reader = new StreamReader(stream))
+            {
+                input = reader.ReadToEnd();
+
+            }
+        }
+
+        public string SolveFirstPuzzle()
+        {
+            var numbers = input.Split("\n").Select(line => int.Parse($"{line.First(c => c >= '0' && c <= '9')}{line.Last(c => c >= '0' && c <= '9')}")).ToList();
+            return numbers.Sum().ToString();
+        }
+
+        public string SolveSecondPuzzle()
+        {
+            var spelledNumbers = new List<string> { "one", "1", "two", "2", "three", "3", "four", "4", "five", "5", "six", "6", "seven", "7", "eight", "8", "nine","9"};
+            var numbers = new List<int>();
+            foreach (var line in input.Split("\r\n"))
+            {
+                var orderedSmallIndexes = spelledNumbers.ToDictionary(k => k, k => line.IndexOf(k)).Where(kvp => kvp.Value > -1).OrderBy(kvp=>kvp.Value);
+                var orderedHighIndexes = spelledNumbers.ToDictionary(k => k, k => line.LastIndexOf(k)).Where(kvp => kvp.Value > -1).OrderBy(kvp => kvp.Value);
+                var firstDigit = spelledNumbers.IndexOf(orderedSmallIndexes.First().Key) / 2 + 1;
+                var lasDigit = spelledNumbers.IndexOf(orderedHighIndexes.Last().Key) / 2 + 1;
+                var theNumber = int.Parse($"{firstDigit}{lasDigit}");
+                numbers.Add(theNumber);
+            }
+            return numbers.Sum().ToString();
+        }
+    }
+}
---
+++ andrei/AdventOfCode2023/Day04.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023
+{
+    internal class Day04
+    {
+        string input;
+
+        public Day04()
+        {
+            var currentAssembly = Assembly.GetExecutingAssembly();
+            var numbers = new List<int>();
+            using (var stream = currentAssembly.GetManifestResourceStream($"{currentAssembly.GetName().Name}.Inputs.Day04.txt"))
+            using (var reader = new StreamReader(stream))
+            {
+                input = reader.ReadToEnd();
+            }
+        }
+
+        public string SolveFirstPuzzle()
+        {
+            var lines = input.Split("\r\n");
+            var ticketsSum = lines.Select(line =>
+            {
+                var parts = line.Split(new char[] { ':', '|' });
+                var r = new
+                {
+                    Ticket = parts[1].Trim().Split(" ").Where(s => s != string.Empty).Select(n => int.Parse(n.Trim())).ToList(),
+                    Numbers = parts[2].Trim().Split(" ").Where(s => s != string.Empty).Select(n => int.Parse(n.Trim())).ToList(),
+                };
+                return r;
+            }).Select(t => {
+                var lucky = t.Ticket.Intersect(t.Numbers);
+                return lucky.Any() ? Math.Pow(2, lucky.Count() - 1) : 0;
+            }
+            ).Sum();
+            return ticketsSum.ToString();
+        }
+
+        public string SolveSecondPuzzle()
+        {
+            var lines = input.Split("\r\n");
+            var allCards = Enumerable.Range(1, lines.Length).ToDictionary(k => k, v => 1);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var parts = lines[i].Split(new char[] { ':', '|' });
+                var Ticket = parts[1].Trim().Split(" ").Where(s => s != string.Empty).Select(n => int.Parse(n.Trim())).ToList();
+                var Numbers = parts[2].Trim().Split(" ").Where(s => s != string.Empty).Select(n => int.Parse(n.Trim())).ToList();
+                var lucky = Ticket.Intersect(Numbers).Count();
+                if(lucky > 0)
+                {
+                    Enumerable.Range(i + 2, lucky).ToList().ForEach(index => {
+                        if (allCards.ContainsKey(index))
+                        {
+                            allCards[index] += allCards[i+1];
+                        }
+                    });
+                }
+            };
+            return allCards.Values.Sum().ToString();
+        }
+    }
+}
